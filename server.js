8fa463945b94e7bb6daa135b81573bbc55b24e46
amir dadipoor <amirdadipoor@hotmail.com>
2025-05-06 10:26:53 +0330

    all new methods to api

--- server.js
+++ server.js
@@ -36,6 +36,66 @@ app.post('/api/v3/lists' , async (req , res) => {
     }
 })
 
+// GET list with all cards
+app.get('/api/v3/lists/:id' , async (req , res) => {
+    try {
+        const list_id = parseInt(req.params.id);
+        const myList = await prisma.list.findUnique(
+            {
+                where: { id: list_id },
+                include: {
+                    cards : true
+                }
+            });
+        if (!myList) return res.status(404).send('Not found');
+        res.json(myList);
+    } catch (err) {
+        res.status(500).send('Error fetching lists ');
+    }
+});
+
+// GET get cards of spec list
+app.get('/api/v3/lists/:id/cards' , async (req , res) => {
+    try {
+        const list_id = parseInt(req.params.id);
+        const myList = await prisma.list.findUnique(
+            {
+                where: { id: list_id },
+                include: {
+                    cards : true
+                }
+            });
+        if (!myList) return res.status(404).send('Not found');
+
+        res.json(myList.cards);
+    } catch (err) {
+        res.status(500).send('Error fetching lists ');
+    }
+});
+
+app.get('/api/v3/cards' , async (req , res) => {
+    try {
+        const allCards = await prisma.card.findMany({});
+        res.json(allCards);
+    } catch (err) {
+        res.status(500).send('Error fetching lists ');
+    }
+});
+
+app.get('/api/v3/cards/:id' , async (req , res) => {
+    try {
+        const allCards = await prisma.card.findUnique({
+            where : {
+                id : parseInt(req.params.id)
+            }
+        });
+        if (!allCards) return res.status(404).send('Not found');
+        res.json(allCards);
+    } catch (err) {
+        res.status(500).send('Error fetching lists ');
+    }
+})
+
 // POST add new card to list
 app.post('/api/v3/lists/:id/cards', async (req, res) => {
 
