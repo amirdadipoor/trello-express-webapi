c01f7faac39f98adbf00926de56f47467dda2d67
amir dadipoor <amirdadipoor@hotmail.com>
2025-05-19 14:55:08 +0330

    add cors headers

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const listRouter = require('./routes/listRouter')
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/v3/lists', listRouter);
 
